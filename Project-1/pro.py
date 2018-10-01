bbf82309a2ab462799c7a5a581492560b76f2c89
yasiraslamshah <yasir.shah@colorado.edu>
2018-10-01 01:28:59 -0600

    Final code

--- Project-1/pro.py
+++ Project-1/pro.py
@@ -27,8 +27,8 @@ listA=[]
 listB=[]
 count1=[]
 count2=[]
-array_average1=[]
-array_average2=[]
+array_average1=[0,0,0,0]
+array_average2=[0,0,0,0]
 global average1
 #function to call calender  
 cal1=calendar.month(2018,10)
@@ -77,6 +77,10 @@ class project(QDialog):
         self.pushButton1.clicked.connect(self.on_pushButton1_clicked)
         self.pushButton2.clicked.connect(self.on_pushButton2_clicked)
         self.pushButton3.clicked.connect(self.on_pushButton3_clicked)
+        humidity,temperature = Adafruit_DHT.read(22,4)
+        if humidity == None:#if the sensor is not connected the console prints an error message
+            self.label_Hum1.setText("Not Connected")
+            self.label_Temp1.setText("Not Connected") 
         self.average_hum()
         self.average_temp()
         self.main_widget = QWidget(self)
@@ -89,6 +93,8 @@ class project(QDialog):
         self.main_widget1.setGeometry(QtCore.QRect(250,450,200,150))
         sc = MyDynamicMplCanvas1(self.main_widget1, width=5, height=4, dpi=100)
         t.addWidget(sc)
+        self.lineEdit.setText("0")
+        self.lineEdit1.setText("0")
     @pyqtSlot()
     #date1=format(today)
     #self.label_Time.setText(str(date))
@@ -102,32 +108,32 @@ class project(QDialog):
             #print(cal1)
             global average1
             #global array_average1[]
-            humidity,temperature = Adafruit_DHT.read_retry(22,4)
+            humidity,temperature = Adafruit_DHT.read(22,4)
             if humidity == None:#if the sensor is not connected the console prints an error message
                 self.label_Hum1.setText("Not Connected")
             else:    
                 #print('Humidity ={0:0.1f}%'.format(humidity))
                 hum=humidity
                 listA.append(hum)                
-            sum1=sum(listA)
-            #print(sum1)
-            #print('Timestamp : {:%H:%M:%S}'.format(datetime.datetime.now()))
-            #print('Date :{:%Y-%m-%d}'.format(datetime.datetime.now()))
-            length1=len(listA)
-            average1=(sum1/length1)
-            array_average1.append(average1)
-            #print('Hum_average is')
-            #print(array_average1)
-            self.label_AveA1.setText(str(average1))
-            a=1
-            count1.append(a)
-            counter1=sum(count1)
-            #print('count is ')
-            #print( counter1)
-            if counter1 == 10:
-                listA.clear()
-                #print('yaha')
-                count1.clear()
+                sum1=sum(listA)
+                #print(sum1)
+                #print('Timestamp : {:%H:%M:%S}'.format(datetime.datetime.now()))
+                #print('Date :{:%Y-%m-%d}'.format(datetime.datetime.now()))
+                length1=len(listA)
+                average1=(sum1/length1)
+                array_average1.append(average1)
+                #print('Hum_average is')
+                #print(array_average1)
+                self.label_AveA1.setText(str(average1))
+                a=1
+                count1.append(a)
+                counter1=sum(count1)
+                #print('count is ')
+                #print( counter1)
+                if counter1 == 10:
+                    listA.clear()
+                    #print('yaha')
+                    count1.clear()
         finally:
             QtCore.QTimer.singleShot(2000,self.average_hum)#the average function for humidity is called after every 2 sec 
     #function to calculate the average temperture values automatically after every 2.5 seconds
@@ -135,31 +141,31 @@ class project(QDialog):
     def average_temp(self):
         try:
             global average2
-            humidity,temperature = Adafruit_DHT.read_retry(22,4)
+            humidity,temperature = Adafruit_DHT.read(22,4)
             #print('yasir')
             if temperature == None:#if the sensor is not connected, an error message is displayed on UI
-                self.label_temp1.setText("Not Connected")
+                self.label_Temp1.setText("Not Connected")
             else:
                 #print('Temperature ={0:0.f}*'.format(temperature))
                 temp=temperature
                 listB.append(temp)
-            sum2=sum(listB)
-            #print(sum2)
-            length2=len(listB)
-            average2=(sum2/length2)
-            array_average2.append(average2)
-            #print('Temp_average')
-            #print(average2)
-            self.label_AveB1.setText(str(average2))
-            b=1
-            count2.append(b)
-            counter2=sum(count2)
-            #print(counter2)
-            #after ten entries the array is cleared
-            if counter2 == 10:
-                listB.clear()
-                #print('waha')
-                count2.clear()
+                sum2=sum(listB)
+                #print(sum2)
+                length2=len(listB)
+                average2=(sum2/length2)
+                array_average2.append(average2)
+                #print('Temp_average')
+                #print(average2)
+                self.label_AveB1.setText(str(average2))
+                b=1
+                count2.append(b)
+                counter2=sum(count2)
+                #print(counter2)
+                #after ten entries the array is cleared
+                if counter2 == 10:
+                    listB.clear()
+                    #print('waha')
+                    count2.clear()
         finally:
             #this function is called to display average temperature every 2.5 seconds
             QtCore.QTimer.singleShot(2500,self.average_temp)
@@ -168,23 +174,23 @@ class project(QDialog):
     def on_pushButton1_clicked(self):
         timer_temp=(datetime.datetime.now().strftime('%H:%M:%S'))
         self.label_Time2.setText(str(timer_temp))
-        humidity,temperature = Adafruit_DHT.read_retry(22,4)
-        farenhiet=((temperature *9)/5 + 32) #for temperature conversion to farenhiet
-        kelvin = temperature+273 #for temperature conversion to kelvin
+        humidity,temperature = Adafruit_DHT.read(22,4)
+        #farenhiet=((temperature *9)/5 + 32) #for temperature conversion to farenhiet
         if temperature == None:#if sensor is not connected, an error message is displayed
-            self.label_temp1.setText("Not Connected")
+            self.label_Temp1.setText("Not Connected")
         else:
-            if flag == 2:#to display values in farenheit
-                self.label_Temp1.setText(str(farenhiet))
-                print("farenhiet")
-            elif flag ==3:#to display values in kelvin
-                self.label_Temp1.setText(str(kelvin))
-                print('kelvin')
-            elif flag ==1:    #to display temperature in celsius
-                self.label_Temp1.setText(str(temperature))
-                print('temperature')
-            print('Temp ={0:0.1f}*'.format(temperature))
+            #print('Temp ={0:0.1f}*'.format(temperature))
             self.label_Temp1.setText(str(temperature))
+            comp2=self.lineEdit1.text()
+            #print(comp2)
+            if comp2 == None:
+                self.label_AlertA.setText('Set Threshold')
+            else:   
+                compare2=int(comp2)
+                if compare2 > temperature:
+                    self.label_AlertB.setText('Alert!')
+                else:
+                    self.label_AlertB.setText('Normal!')
             temp=temperature
             listB.append(temp)
     #function defined at pushbutton clicked(Refresh) to display the current humidity.The humidity 
@@ -193,7 +199,7 @@ class project(QDialog):
         #displaying time stamp
         timer_hum =(datetime.datetime.now().strftime('%H:%M:%S'))
         self.label_Time1.setText(str(timer_hum))
-        humidity,temperature = Adafruit_DHT.read_retry(22,4)
+        humidity,temperature = Adafruit_DHT.read(22,4)
         if humidity == None:
             #if the humidty sensor is not connected an error is displayed as not connected
             self.label_Hum1.setText("Not Connected")
@@ -202,35 +208,38 @@ class project(QDialog):
             #print('Humidity ={0:0.1f}%'.format(humidity))
             hum=humidity
             listA.append(hum)
-            comp1=self.lineEdit.text())
+            comp1=self.lineEdit.text()
+            #print(comp1)
+            #compare1 = int(comp1)
             if comp1 == None:
                 self.label_AlertA.setText('Set Threshold')
-            else:    
-                if comp1 >humidity:
+            else:
+                compare1=int(comp1)
+                if compare1 > humidity:
                     self.label_AlertA.setText('Alert!')
                 else:
-                    self.label_Alert.setText('Normal!')
+                    self.label_AlertA.setText('Normal!')
     #pushbutton 3 is used to display the current temperature in farenheit        
     def on_pushButton3_clicked(self):
-        humidity,temperature = Adafruit_DHT.read_retry(22,4)
+        humidity,temperature = Adafruit_DHT.read(22,4)
         if temperature == None:
-            self.label_temp1.setText("Not Connected")
+            self.label_Temp1.setText("Not Connected")
         else:
-            farenhiet=((temperature *9)/5 + 32)
+            farenhiet=((temperature*9)/5 + 32)
             self.label_Temp1.setText(str(farenhiet))
     #pushbutton 4 is used to display the current temperature in kelvin     
     def on_pushButton4_clicked(self):
-        humidity,temperature = Adafruit_DHT.read_retry(22,4)
+        humidity,temperature = Adafruit_DHT.read(22,4)
         if temperature == None:
-            self.label_temp1.setText("Not Connected")
+            self.label_Temp1.setText("Not Connected")
         else:
             kelvin=temperature+273 
             self.label_Temp1.setText(str(kelvin))
     #pushbutton 5 is clicked to display temperature in celsius     
     def on_pushButton5_clicked(self):
-        humidity,temperature = Adafruit_DHT.read_retry(22,4)
+        humidity,temperature = Adafruit_DHT.read(22,4)
         if temperature == None:
-            self.label_temp1.setText("Not Connected")
+            self.label_Temp1.setText("Not Connected")
         else:
             self.label_Temp1.setText(str(temperature)) 
 
